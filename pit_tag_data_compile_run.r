63d4e96512275468dccbe3ef664c276441ecd10a
Justin Braaten <jstnbraaten@gmail.com>
2018-05-30 10:48:50 -0700

    changed the list file from 2 to 1

--- pit_tag_data_compile_run.r
+++ pit_tag_data_compile_run.r
@@ -5,7 +5,7 @@
 
 functionPath = "C:\\Users\\braatenj\\Documents\\GitHub\\pit-tag-data-compile\\pit_tag_data_compile_functions.r"
 DBfile = "C:\\Users\\braatenj\\Documents\\GitHub\\pit-tag-data-compile\\example\\pitTagDB.csv"
-inputFileList = "C:\\Users\\braatenj\\Documents\\GitHub\\pit-tag-data-compile\\example\\fileList2.csv"
+inputFileList = "C:\\Users\\braatenj\\Documents\\GitHub\\pit-tag-data-compile\\example\\fileList1.csv"
 timeZone = "America/Los_Angeles"
 
 #####################################################################################################################
