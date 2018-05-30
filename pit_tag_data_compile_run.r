455b58f7380ce51e1be28700eaa585d425c7eeac
Justin Braaten <jstnbraaten@gmail.com>
2018-05-29 22:24:54 -0700

    initial commit

---
+++ pit_tag_data_compile_run.r
@@ -0,0 +1,16 @@
+
+#####################################################################################################################
+### INPUTS ##########################################################################################################
+#####################################################################################################################
+
+functionPath = "C:\\Users\\braatenj\\Documents\\GitHub\\pit-tag-data-compile\\pit_tag_data_compile_functions.r"
+DBfile = "C:\\Users\\braatenj\\Documents\\GitHub\\pit-tag-data-compile\\example\\pitTagDB.csv"
+inputFileList = "C:\\Users\\braatenj\\Documents\\GitHub\\pit-tag-data-compile\\example\\fileList2.csv"
+timeZone = "America/Los_Angeles"
+
+#####################################################################################################################
+
+
+
+source(functionPath)  
+PITcompile(DBfile, inputFileList, timeZone)
